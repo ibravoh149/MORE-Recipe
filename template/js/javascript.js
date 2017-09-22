5a01737e91f2fffba5199459b86f64aac7e5c2d1
Ibrahim iliyas <ibravoh149@gmail.com>
2017-09-23 00:26:21 +0100

    second commit with added pages: profile, dashboard, favourite

--- template/js/javascript.js
+++ template/js/javascript.js
@@ -2,4 +2,8 @@ $( document ).ready(function(){
 	$(".button-collapse").sideNav();
 	 $('.carousel').carousel();
 	 $('.carousel').carousel('set', 4);
+	  $('.parallax').parallax();
+
+
+	  $('.modal').modal();
 });
