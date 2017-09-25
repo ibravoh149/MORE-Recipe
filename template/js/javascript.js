1e42101ddb4e72a6b8d5ccad2890d486b577a612
Ibrahim iliyas <ibravoh149@gmail.com>
2017-09-25 02:14:54 +0100

    bug fix

--- template/js/javascript.js
+++ template/js/javascript.js
@@ -6,4 +6,10 @@ $( document ).ready(function(){
 
 
 	  $('.modal').modal();
+
+
+	  //document.getElementById('signup').addEventListener('click', ()=>{
+	  	//	window.location.href ='dashboard.html';
+	  //});
+
 });
